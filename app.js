3ff337492d3edb2463c66cb14311013ff683a5e5
Tri Vo <vocaotri445@gmail.com>
2020-12-25 14:15:40 +0700

    add subscription

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 require('dotenv').config();
 const express = require("express");
-const {buildSchema} = require("graphql");
-const {graphqlHTTP} = require("express-graphql");
+const { createServer } = require('http');
+const { execute, subscribe, buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
 const { importSchema } = require("graphql-import");
+const { SubscriptionServer } = require('subscriptions-transport-ws');
 const graphqlResolvers = require("./graphql/resolvers");
 const mongoose = require("mongoose");
+
+const PORT = 3000;
+const subscriptionsEndpoint = `ws://localhost:${PORT}/subscriptions`;
 const server = express();
+const schema = buildSchema(importSchema("**/*.graphql"));
 server.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: buildSchema(importSchema("**/*.graphql")),
-    rootValue: graphqlResolvers,
-    graphiql: true,
-  })
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: graphqlResolvers,
+        endpointURL: '/graphql',
+        graphiql: {
+            subscriptionsEndpoint: subscriptionsEndpoint
+        }
+    })
 );
-// const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uox7n.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const webServer = createServer(server);
+
 const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+
 mongoose
-  .connect(uri, options)
-  .then(() => server.listen(3000, console.log("Server is running")))
-  .catch((error) => {
-    throw error;
-  });
+    .connect(uri, options)
+    .then(() => webServer.listen(PORT, () => {
+        console.log(`GraphQL is now running on http://localhost:${PORT}`);
+
+        // Set up the WebSocket for handling GraphQL subscriptions.
+        new SubscriptionServer({
+            execute,
+            subscribe,
+            schema
+        }, {
+            server: webServer,
+            path: '/subscriptions',
+        })
+    }))
+    .catch((error) => {
+        throw error;
+    });
