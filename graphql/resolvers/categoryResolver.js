f429283fd8a5a5f0ec8593287697b50ac59d9882
Tri Vo <vocaotri445@gmail.com>
2020-12-25 10:20:47 +0700

    update check token

--- graphql/resolvers/categoryResolver.js
+++ graphql/resolvers/categoryResolver.js
@@ -1,43 +1,50 @@
 const Category = require("../../models/category");
+const authJWT = require('../../util/function');
 module.exports = {
-  categories: async () => {
-    try {
-      const categoryFetched = await Category.find().populate("articles");
-      console.log(categoryFetched);
-      return categoryFetched.map((category) => {
-        return {
-          ...category._doc,
-          _id: category.id,
-          createdAt: new Date(category._doc.createdAt).toISOString(),
-        };
-      });
-    } catch (error) {
-      throw error;
-    }
-  },
+    categories: async() => {
+        try {
+            const categoryFetched = await Category.find().populate("articles");
+            console.log(categoryFetched);
+            return categoryFetched.map((category) => {
+                return {
+                    ...category._doc,
+                    _id: category.id,
+                    createdAt: new Date(category._doc.createdAt).toISOString(),
+                };
+            });
+        } catch (error) {
+            throw error;
+        }
+    },
 
-  createCategory: async (args) => {
-    try {
-      const { name } = args.category;
-      const category = new Category({
-        name,
-      });
-      const newCategory = await category.save();
-      return { ...newCategory._doc, _id: newCategory.id };
-    } catch (error) {
-      throw error;
-    }
-  },
+    createCategory: async(args, context) => {
+        try {
+            const { name } = args.category;
+            const category = new Category({
+                name,
+            });
+            const token = context.headers.authorization.split(" ")[1];
+            const user = await authJWT.verifyAccessToken(token);
+            if (Object.keys(user).length > 0) {
+                const newCategory = await category.save();
+                return {...newCategory._doc, _id: newCategory.id };
+            } else
+                throw new Error("Token failed")
 
-  updateCategory: async (args) => {
-    try {
-      const { _id, name } = args.category;
-      const category = await Category.findById(_id);
-      category.name = name;
-      const newCategory = await category.save();
-      return { ...newCategory._doc, _id: newCategory.id };
-    } catch (error) {
-      throw error;
-    }
-  },
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    updateCategory: async(args) => {
+        try {
+            const { _id, name } = args.category;
+            const category = await Category.findById(_id);
+            category.name = name;
+            const newCategory = await category.save();
+            return {...newCategory._doc, _id: newCategory.id };
+        } catch (error) {
+            throw error;
+        }
+    },
 };
--- util/function.js
+++ util/function.js
@@ -1,12 +1,17 @@
+const userModel = require('../models/user');
 const jwt = require("jsonwebtoken");
 module.exports = {
-  generateAccessToken: (data) => {
-    return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '7889232s' });
-  },
-  verifyAccessToken:(token)=>{
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err, user) => {
-        if (err) return 0
-        return user;
-      })
-  }
+    generateAccessToken: (data) => {
+        return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '7889232s' });
+    },
+    verifyAccessToken: async(token) => {
+        let user = await jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+            if (err) return null;
+            return user;
+        })
+        if (user === null)
+            return {};
+        user = await userModel.findById(user.id);
+        return {...user };
+    }
 };
